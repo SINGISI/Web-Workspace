f9eea1ec8d76539e944f2e66b1f4d05e72d877da
singisi <singisi.josseph@gmail.com>
2022-05-10 16:53:52 +0300

    basic form

---
+++ forms/form.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+  <title>BASIC FORM </title>
+<link rel="stylesheet" href="../styles/style.css">
+<link rel="stylesheet" href="../styles/bootstrap.min.css">
+</head>
+<body>
+  <form>
+    <p style="color:blue">Registration form</p>
+<label for="name "> Name</label><br> <input type="text" name="name">
+<br>
+<label for="email ">Email </label><br> <input type="email" name="email">
+<br>
+  <label for="password">Password</label><br><input type="password" name="password"><br>
+<p><input type="submit" name="submit" value="submit"></p>
+</form>
+</body>
+</html>
